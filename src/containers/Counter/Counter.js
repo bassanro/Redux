98732411b84a20aa1c85623e9b9a9c4c6d506f74
ROSHAN BASSAN <roshan.bassan@gmail.com>
2019-12-05 11:35:48 +1100

    Combining multiple reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -21,7 +21,9 @@ class Counter extends Component {
         <CounterControl label='Add 5' clicked={this.props.onAddCounter} />
         <CounterControl label='Subtract 5' clicked={this.props.onSubCounter} />
         <hr />
-        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>
+          Store Result
+        </button>
         <ul>
           {this.props.storedResults.map(strResult => (
             <li
@@ -41,8 +43,8 @@ class Counter extends Component {
 // This is maps to props.
 const mapStateToProps = state => {
   return {
-    ctr: state.counter,
-    storedResults: state.results
+    ctr: state.ctr.counter,
+    storedResults: state.res.results
   };
 };
 
@@ -53,7 +55,8 @@ const mapDispatchToProps = dispatch => {
     onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
     onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 5 }),
     onSubCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 5 }),
-    onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+    onStoreResult: result =>
+      dispatch({ type: actionTypes.STORE_RESULT, result: result }),
     onDeleteResult: id =>
       dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
   };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
-import { Provider }  from "react-redux"
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from "./store/reducer";
+import counterReducer from './store/reduers/counter';
+import resultReducer from './store/reduers/result';
 
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
-// Provier is helper container to inject our stores in react components. 
-// </Provider> for hooking up provider container with app wrap it with createStore. 
-ReactDOM.render( <Provider store={store}> <App /></Provider>, document.getElementById('root'));
+// Provier is helper container to inject our stores in react components.
+// </Provider> for hooking up provider container with app wrap it with createStore.
+ReactDOM.render(
+  <Provider store={store}>
+    {' '}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
