7d3f817887a207c56aa75a7865d1f72c28b36029
ROSHAN BASSAN <roshan.bassan@gmail.com>
2019-12-06 16:40:14 +1100

    Setting up middleware and Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -13,7 +13,25 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-const store = createStore(rootReducer);
+// Function which returns function.
+const logger = store => {
+  return next => {
+    return action => {
+      // Middleware
+      console.log('[Middleware] Dispathing.', action);
+      const result = next(action);
+      console.log('[Middleware] next state.', store.getState());
+      return result;
+    };
+  };
+};
+
+// For Redux DevTools in Chrome.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 // Provier is helper container to inject our stores in react components.
 // </Provider> for hooking up provider container with app wrap it with createStore.
